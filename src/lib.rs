f2d9dddd7dc8a589dc49dd43915443e0fa919d7d
Harshit Mittal <52619772+harsmitt@users.noreply.github.com>
2026-09-12 13:44:28 +0530

    Register Kafka producer function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod kafka_metadata;
+mod kafka_produce;
 mod kafka_scan;
 
 use duckdb::{duckdb_entrypoint_c_api, Connection, Result};
@@ -8,5 +9,6 @@ use std::error::Error;
 pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
     con.register_table_function::<kafka_scan::KafkaScan>("tributary_scan_topic")?;
     con.register_table_function::<kafka_metadata::KafkaMetadata>("tributary_metadata")?;
+    con.register_table_function::<kafka_produce::KafkaProduce>("tributary_produce")?;
     Ok(())
 }
